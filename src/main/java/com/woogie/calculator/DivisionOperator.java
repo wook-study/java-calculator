05e4e2e9b63fa9400ad4537f3a0e8aa977f47fab
Woogie Kim <mdir2kor@gmail.com>
2022-03-21 20:49:24 +0900

    feat: 곱하기 오퍼레이터 추가

---
+++ src/main/java/com/woogie/calculator/DivisionOperator.java
@@ -0,0 +1,25 @@
+package com.woogie.calculator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class DivisionOperator implements Operable {
+
+    private final int scale;
+    private final RoundingMode roundingMode;
+
+    public DivisionOperator() {
+        this.scale = 0;
+        this.roundingMode = RoundingMode.HALF_UP;
+    }
+
+    public DivisionOperator(int scale, RoundingMode roundingMode) {
+        this.scale = scale;
+        this.roundingMode = roundingMode;
+    }
+
+    @Override
+    public BigDecimal execute(BigDecimal augend, BigDecimal addend) {
+        return augend.divide(addend, scale, roundingMode);
+    }
+}
