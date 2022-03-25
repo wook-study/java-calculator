ab32bf7972e34e99dfa31e5e75d66c2b60afc8e3
Woogie Kim <mdir2kor@gmail.com>
2022-03-25 13:00:48 +0900

    docs: 주석 추가

--- src/main/java/com/woogie/calculator/Application.java
+++ src/main/java/com/woogie/calculator/Application.java
@@ -6,7 +6,6 @@ import com.woogie.calculator.component.RegExExpressionValidator;
 import com.woogie.calculator.component.StringToExpressionParser;
 import com.woogie.calculator.ui.CalculatorBehavior;
 import com.woogie.calculator.ui.console.ConsoleCalculatorBehavior;
-import com.woogie.calculator.ui.console.InputView;
 
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/woogie/calculator/ui/AbstractCalculatorBehavior.java
+++ src/main/java/com/woogie/calculator/ui/AbstractCalculatorBehavior.java
@@ -1,12 +1,27 @@
 package com.woogie.calculator.ui;
 
+/**
+ * 계산기 어플리케이션의 추상클래스 (실행에 필요한 기능을 모아 템플릿화 함)
+ */
 public abstract class AbstractCalculatorBehavior implements CalculatorBehavior {
+    /**
+     * 계산기가 최초 실행하면 동작
+     */
     abstract protected void doOnStart();
 
+    /**
+     * 계산기 실행이 완료되면 동작
+     */
     abstract protected void doOnComplete();
 
+    /**
+     * 예외가 발생했을때 동작
+     */
     abstract protected void doOnError(RuntimeException ex);
 
+    /**
+     * 계산기 실행 완료 여부를 결정
+     */
     abstract protected boolean complete();
 
     @Override
